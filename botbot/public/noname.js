e636b169660c7299ec9b8f911145596dd6b82524
Yoda <Yoda@DESKTOP-7MKVKML>
2018-01-11 17:48:03 -0600

    another test...nothing done but a comment

---
+++ botbot/public/noname.js
@@ -0,0 +1,11 @@
+/**
+ * http://usejsdoc.org/
+ */
+{
+	"bot_id" :"a768b8cbb314f1cdbe080ab86f",
+	"text"   : "I dont do anything cool yet...you dont like it?...fight me irl"
+}
+function getRandom(){ //d20
+	min = 1; max = Math.floor(21); 
+	return Math.floor(Math.random() * (max - min)) + min;  
+}
