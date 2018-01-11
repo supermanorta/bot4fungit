70124c816c623279cc18b225bbe020f4beee1b2e
Yoda <Yoda@DESKTOP-7MKVKML>
2018-01-11 17:43:50 -0600

    1st commit, might have broken something...we will see

---
+++ botbot/public/app.js
@@ -0,0 +1,15 @@
+
+var app = angular.module('MyAngularApp',[]);
+//var app = angular.module('MyAngularApp', [], function($httpProvider) {
+//	  // Use x-www-form-urlencoded Content-Type
+//	  //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+//});
+//not sure whats above....mayeb delete it later
+//review what this does, the demo uses this not the tom hanks picture in the link...
+//{
+//	  "name": "Cool guy bot",
+//	  "description": "Basic Groupme Bot written in Node",
+//	  "repository": "https://github.com/petemcgrath/cool-guy-bot",
+//	  "logo": "http://www.tailgate365.com/wp-content/uploads/2011/06/tom-hanks-906.jpg",
+//	  "keywords": []
+//	}
---
+++ botbot/public/index.js
@@ -0,0 +1,35 @@
+var http, director, cool, bot, router, server, port;
+
+http        = require('http');
+director    = require('director');
+cool        = require('cool-ascii-faces');
+bot         = require('./bot.js');
+
+router = new director.http.Router({
+  '/' : {
+    post: bot.respond,
+    get: ping
+  }
+});
+
+server = http.createServer(function (req, res) {
+  req.chunks = [];
+  req.on('data', function (chunk) {
+    req.chunks.push(chunk.toString());
+  });
+
+  router.dispatch(req, res, function(err) {
+    res.writeHead(err.status, {"Content-Type": "text/plain"});
+    res.end(err.message);
+  });
+});
+
+port = Number(process.env.PORT || 5000);
+server.listen(port);
+//change ping later to my own.
+function ping() {
+  this.res.writeHead(200);
+  this.res.end("Hey, I'm Cool Guy. Get with this for freeski");
+  
+  
+}
